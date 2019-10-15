51c4f808da3ffd7b037484b00d35fcc5fd95be63
Connor Sullivan <cksullvn@gmail.com>
2019-10-15 16:20:36 -0500

    updated js and scss to print color cards with hover functionality to display their names

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,15 @@
 import 'bootstrap';
-import cardList from './components/cardList/cardList';
 // import arrayMethods from './arrayMethods';
+import cardList from './components/cardList/cardList';
+import c from './helpers/data/colors';
+
 import '../styles/main.scss';
 
 const init = () => {
   // arrayMethods.init();
-  cardList.cardListBuilder();
+  const colors = c.getColors();
+  cardList.cardListEvents();
+  cardList.cardListBuilder(colors);
 };
 
 init();
